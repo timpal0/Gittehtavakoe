b76551592d3e3f7d68c05155387149bfd67c4d25
Palomaa <Timo.Palomaa@seamk.fi>
2018-10-23 15:38:34 +0300

    uudet rivit lisätty

--- Program.cs
+++ Program.cs
@@ -6,7 +6,7 @@ namespace GitTehtava
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Hei maailma!");
         }
     }
 }
